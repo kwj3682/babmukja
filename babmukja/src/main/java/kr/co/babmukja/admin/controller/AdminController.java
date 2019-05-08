225166780cadd4541fbed4e0dae05332e048b509
Woojoong Kim <44341196+kwj3682@users.noreply.github.com>
2019-05-08 11:34:48 +0900

    AdminController 수정

--- babmukja/src/main/java/kr/co/babmukja/admin/controller/AdminController.java
+++ babmukja/src/main/java/kr/co/babmukja/admin/controller/AdminController.java
@@ -1,10 +1,15 @@
 package kr.co.babmukja.admin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.babmukja.admin.service.AdminService;
+import kr.co.babmukja.repository.domain.Member;
 
 @Controller("kr.co.babmukja.admin.controller.AdminController")
 @RequestMapping("/admin")
@@ -17,7 +22,11 @@ public class AdminController {
 	public void main(){}
 	
 	@RequestMapping("/memberlist.do")
-	public void list(){
-		service.listMember();
+	public ResponseEntity<String> list(){
+		List<Member> list = service.listMember();
+		if(list == null) {
+			return new ResponseEntity<String>("회원 테이블을 가져오는데 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);			
+		}
+		return new ResponseEntity<String> ("검색 결과 : 총"+list.size()+"명의 회원 정보를 검색했습니다.",HttpStatus.OK);
 	}
 }
