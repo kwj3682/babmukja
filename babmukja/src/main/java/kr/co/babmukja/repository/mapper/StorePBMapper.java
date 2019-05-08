402fd5831085b160ef636bf8712a5f0cdd7c3140
qlccks789 <qlccks789@naver.com>
2019-05-08 10:14:59 +0900

    Revert "store 업로드"
    This reverts commit 51bb95139232a215a0a22b5046d5673a2fed889b.

--- babmukja/src/main/java/kr/co/babmukja/repository/mapper/StorePBMapper.java
+++
@@ -1,5 +0,0 @@
-package kr.co.babmukja.repository.mapper;
-
-public class StorePBMapper {
-
-}
--- babmukja/src/main/java/kr/co/mlec/babmukja/store/service/StorePBService.java
+++
@@ -1,6 +0,0 @@
-package kr.co.mlec.babmukja.store.service;
-
-// StorePBServiceImpl 에 있는 메서드를 선언한다.
-public interface StorePBService {
-
-}
