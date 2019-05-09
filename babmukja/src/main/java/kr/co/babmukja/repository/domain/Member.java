bb5682cae83b05dbed348479b9206a9a2275023a
flamefjqm <jyjtvxq777@naver.com>
2019-05-09 17:10:07 +0900

    membervo

--- babmukja/src/main/java/kr/co/babmukja/repository/domain/Member.java
+++ babmukja/src/main/java/kr/co/babmukja/repository/domain/Member.java
@@ -3,137 +3,7 @@ package kr.co.babmukja.repository.domain;
 import java.util.Date;
 
 public class Member {
-<<<<<<< HEAD
-	private int memNo;
-	private String memName;
-	private String memPhone;
-	private String memEmail;
-	private String memId;
-	private String memNickname;
-	private String memPass;
-	private String postNo;
-	private String addrDefault;
-	private String addrDetail;
-	private char managerAt;
-	private String imgOrgname;
-	private String imgSysname;
-	private Date signDate;
-	private Date lastDate;
-	private int socialAt;
-	private int point;
-	private int gradeNo;
-	private int hintNo;
-	private String hintAnswer;
-	private String imgPath;
-	public int getMemNo() {
-		return memNo;
-	}
-	public void setMemNo(int memNo) {
-		this.memNo = memNo;
-	}
-	public String getMemName() {
-		return memName;
-	}
-	public void setMemName(String memName) {
-		this.memName = memName;
-	}
-	public String getMemPhone() {
-		return memPhone;
-	}
-	public void setMemPhone(String memPhone) {
-		this.memPhone = memPhone;
-	}
-	public String getMemEmail() {
-		return memEmail;
-	}
-	public void setMemEmail(String memEmail) {
-		this.memEmail = memEmail;
-	}
-	public String getMemId() {
-		return memId;
-	}
-	public void setMemId(String memId) {
-		this.memId = memId;
-	}
-	public String getMemNickname() {
-		return memNickname;
-	}
-	public void setMemNickname(String memNickname) {
-		this.memNickname = memNickname;
-	}
-	public String getMemPass() {
-		return memPass;
-	}
-	public void setMemPass(String memPass) {
-		this.memPass = memPass;
-	}
-	public String getPostNo() {
-		return postNo;
-	}
-	public void setPostNo(String postNo) {
-		this.postNo = postNo;
-	}
-	public String getAddrDefault() {
-		return addrDefault;
-	}
-	public void setAddrDefault(String addrDefault) {
-		this.addrDefault = addrDefault;
-	}
-	public String getAddrDetail() {
-		return addrDetail;
-	}
-	public void setAddrDetail(String addrDetail) {
-		this.addrDetail = addrDetail;
-	}
-	public char getManagerAt() {
-		return managerAt;
-	}
-	public void setManagerAt(char managerAt) {
-		this.managerAt = managerAt;
-	}
-	public String getImgOrgname() {
-		return imgOrgname;
-	}
-	public void setImgOrgname(String imgOrgname) {
-		this.imgOrgname = imgOrgname;
-	}
-	public String getImgSysname() {
-		return imgSysname;
-	}
-	public void setImgSysname(String imgSysname) {
-		this.imgSysname = imgSysname;
-	}
-	public Date getSignDate() {
-		return signDate;
-	}
-	public void setSignDate(Date signDate) {
-		this.signDate = signDate;
-	}
-	public Date getLastDate() {
-		return lastDate;
-	}
-	public void setLastDate(Date lastDate) {
-		this.lastDate = lastDate;
-	}
-	public int getSocialAt() {
-		return socialAt;
-	}
-	public void setSocialAt(int socialAt) {
-		this.socialAt = socialAt;
-	}
-	public int getPoint() {
-		return point;
-	}
-	public void setPoint(int point) {
-		this.point = point;
-	}
-	public int getGradeNo() {
-		return gradeNo;
-	}
-	public void setGradeNo(int gradeNo) {
-		this.gradeNo = gradeNo;
-	}
-=======
+
    private int memNo;
    private String memName;
    private String memPhone;
@@ -160,9 +30,151 @@ public class Member {
    private String gradeName;
    private int totalBuy;
    private int totalSel;
-   	
-   
-   public int getTotalBuy() {
+public int getMemNo() {
+	return memNo;
+}
+public void setMemNo(int memNo) {
+	this.memNo = memNo;
+}
+public String getMemName() {
+	return memName;
+}
+public void setMemName(String memName) {
+	this.memName = memName;
+}
+public String getMemPhone() {
+	return memPhone;
+}
+public void setMemPhone(String memPhone) {
+	this.memPhone = memPhone;
+}
+public String getMemEmail() {
+	return memEmail;
+}
+public void setMemEmail(String memEmail) {
+	this.memEmail = memEmail;
+}
+public String getMemId() {
+	return memId;
+}
+public void setMemId(String memId) {
+	this.memId = memId;
+}
+public String getMemNickname() {
+	return memNickname;
+}
+public void setMemNickname(String memNickname) {
+	this.memNickname = memNickname;
+}
+public String getMemPass() {
+	return memPass;
+}
+public void setMemPass(String memPass) {
+	this.memPass = memPass;
+}
+public String getPostNo() {
+	return postNo;
+}
+public void setPostNo(String postNo) {
+	this.postNo = postNo;
+}
+public String getAddrDefault() {
+	return addrDefault;
+}
+public void setAddrDefault(String addrDefault) {
+	this.addrDefault = addrDefault;
+}
+public String getAddrDetail() {
+	return addrDetail;
+}
+public void setAddrDetail(String addrDetail) {
+	this.addrDetail = addrDetail;
+}
+public char getManagerAt() {
+	return managerAt;
+}
+public void setManagerAt(char managerAt) {
+	this.managerAt = managerAt;
+}
+public String getImgOrgname() {
+	return imgOrgname;
+}
+public void setImgOrgname(String imgOrgname) {
+	this.imgOrgname = imgOrgname;
+}
+public String getImgSysname() {
+	return imgSysname;
+}
+public void setImgSysname(String imgSysname) {
+	this.imgSysname = imgSysname;
+}
+public Date getSignDate() {
+	return signDate;
+}
+public void setSignDate(Date signDate) {
+	this.signDate = signDate;
+}
+public Date getLastDate() {
+	return lastDate;
+}
+public void setLastDate(Date lastDate) {
+	this.lastDate = lastDate;
+}
+public int getSocialAt() {
+	return socialAt;
+}
+public void setSocialAt(int socialAt) {
+	this.socialAt = socialAt;
+}
+public int getPoint() {
+	return point;
+}
+public void setPoint(int point) {
+	this.point = point;
+}
+public int getHintNo() {
+	return hintNo;
+}
+public void setHintNo(int hintNo) {
+	this.hintNo = hintNo;
+}
+public String getHintAnswer() {
+	return hintAnswer;
+}
+public void setHintAnswer(String hintAnswer) {
+	this.hintAnswer = hintAnswer;
+}
+public String getImgPath() {
+	return imgPath;
+}
+public void setImgPath(String imgPath) {
+	this.imgPath = imgPath;
+}
+public int getGradeNo() {
+	return gradeNo;
+}
+public void setGradeNo(int gradeNo) {
+	this.gradeNo = gradeNo;
+}
+public int getRecipeCnt() {
+	return recipeCnt;
+}
+public void setRecipeCnt(int recipeCnt) {
+	this.recipeCnt = recipeCnt;
+}
+public int getSellingCnt() {
+	return sellingCnt;
+}
+public void setSellingCnt(int sellingCnt) {
+	this.sellingCnt = sellingCnt;
+}
+public String getGradeName() {
+	return gradeName;
+}
+public void setGradeName(String gradeName) {
+	this.gradeName = gradeName;
+}
+public int getTotalBuy() {
 	return totalBuy;
 }
 public void setTotalBuy(int totalBuy) {
@@ -174,149 +186,7 @@ public int getTotalSel() {
 public void setTotalSel(int totalSel) {
 	this.totalSel = totalSel;
 }
-public String getGradeName() {
-	return gradeName;
-   }
-   public void setGradeName(String gradeName) {
-	this.gradeName = gradeName;
-   }	
-	public int getGradeNo() {
-		return gradeNo;
-	}
-   public void setGradeNo(int gradeNo) {
-		this.gradeNo = gradeNo;
-	}
-	public int getRecipeCnt() {
-		return recipeCnt;
-	}
-	public void setRecipeCnt(int recipeCnt) {
-		this.recipeCnt = recipeCnt;
-	}
-	public int getSellingCnt() {
-		return sellingCnt;
-	}
-   public void setSellingCnt(int sellingCnt) {
-	  this.sellingCnt = sellingCnt;
-   }
-   public int getMemNo() {
-	  return memNo;
-   }
-   public void setMemNo(int memNo) {
-      this.memNo = memNo;
-   }
-   public String getMemName() {
-      return memName;
-   }
-   public void setMemName(String memName) {
-      this.memName = memName;
-   }
-   public String getMemPhone() {
-      return memPhone;
-   }
-   public void setMemPhone(String memPhone) {
-      this.memPhone = memPhone;
-   }
-   public String getMemEmail() {
-      return memEmail;
-   }
-   public void setMemEmail(String memEmail) {
-      this.memEmail = memEmail;
-   }
-   public String getMemId() {
-      return memId;
-   }
-   public void setMemId(String memId) {
-      this.memId = memId;
-   }
-   public String getMemNickname() {
-      return memNickname;
-   }
-   public void setMemNickname(String memNickname) {
-      this.memNickname = memNickname;
-   }
-   public String getMemPass() {
-      return memPass;
-   }
-   public void setMemPass(String memPass) {
-      this.memPass = memPass;
-   }
-   public String getPostNo() {
-      return postNo;
-   }
-   public void setPostNo(String postNo) {
-      this.postNo = postNo;
-   }
-   public String getAddrDefault() {
-      return addrDefault;
-   }
-   public void setAddrDefault(String addrDefault) {
-      this.addrDefault = addrDefault;
-   }
-   public String getAddrDetail() {
-      return addrDetail;
-   }
-   public void setAddrDetail(String addrDetail) {
-      this.addrDetail = addrDetail;
-   }
-   public char getManagerAt() {
-      return managerAt;
-   }
-   public void setManagerAt(char managerAt) {
-      this.managerAt = managerAt;
-   }
-   public String getImgOrgname() {
-      return imgOrgname;
-   }
-   public void setImgOrgname(String imgOrgname) {
-      this.imgOrgname = imgOrgname;
-   }
-   public String getImgSysname() {
-      return imgSysname;
-   }
-   public void setImgSysname(String imgSysname) {
-      this.imgSysname = imgSysname;
-   }
-   public Date getSignDate() {
-      return signDate;
-   }
-   public void setSignDate(Date signDate) {
-      this.signDate = signDate;
-   }
-   public Date getLastDate() {
-      return lastDate;
-   }
-   public void setLastDate(Date lastDate) {
-      this.lastDate = lastDate;
-   }
-   public int getSocialAt() {
-      return socialAt;
-   }
-   public void setSocialAt(int socialAt) {
-      this.socialAt = socialAt;
-   }
-   public int getPoint() {
-      return point;
-   }
-   public void setPoint(int point) {
-      this.point = point;
-   }
->>>>>>> 93f72c8bb0c3c7cc9128a59cdf01d6b7c8d6421f
-	public int getHintNo() {
-		return hintNo;
-	}
-	public void setHintNo(int hintNo) {
-		this.hintNo = hintNo;
-	}
-	public String getHintAnswer() {
-		return hintAnswer;
-	}
-	public void setHintAnswer(String hintAnswer) {
-		this.hintAnswer = hintAnswer;
-	}
-	public String getImgPath() {
-		return imgPath;
-	}
-	public void setImgPath(String imgPath) {
-		this.imgPath = imgPath;
-	}
+   
+   
+   
 }
