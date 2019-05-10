fe1c26ed284646bbba9d49871fffecbb33a555a0
qlccks789 <qlccks789@naver.com>
2019-05-10 13:01:09 +0900

    파일 업로드 및 다운로드

--- babmukja/src/main/java/kr/co/babmukja/repository/mapper/StorePBMapper.java
+++ babmukja/src/main/java/kr/co/babmukja/repository/mapper/StorePBMapper.java
@@ -10,4 +10,5 @@ public interface StorePBMapper {
 	public void insertpb(StorePB storepb);
 	public void insertPBImage(FileVO fileVO);
 	public int selectMaxNum();
+	public List<StorePB> selectPBMainList();
 }
